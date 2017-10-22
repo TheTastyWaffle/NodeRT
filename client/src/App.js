8fbaa0c54a8226bb9cee8475489ddac494ced684
Thomas Josso <thomas.josso@gmail.com>
2017-10-22 12:22:26 +0200

    ADD client app with basic message reception

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './App.css';
+import socketIOClient from "socket.io-client";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            response: false,
+            clients: false,
+            endpoint: "http://localhost:3001"
+        };
+    }
+
+    componentDidMount() {
+        const {endpoint} = this.state;
+        const socket = socketIOClient(endpoint);
+        socket.on("message", data => this.setState({message: data}));
+        socket.on("clients", data => this.setState({clients: data}))
+    }
+
+    render() {
+        const {message} = this.state;
+        const {clients} = this.state;
+        return (
+            <div>
+                <div style={{textAlign: "center"}}>
+                    {
+                        clients ?
+                        <p> Number of clients connected: {clients}</p> :
+                        <p> Scanning *bleep* *bloop*</p>
+                    }
+                </div>
+                <div style={{textAlign: "center"}}>
+                    {
+                        message ?
+                            <p> Message: {message}</p> :
+                            <p> No message :( </p>
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
